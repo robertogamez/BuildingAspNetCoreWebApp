16ba68f4ea591a11f29f6978e7034833e0538972
Roberto G <roberto.manuel.balderas.gamez@hotmail.com>
2018-09-16 17:26:19 -0500

    Agregar archivos de proyecto.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(
+            IPieRepository pieRepository,
+            ICategoryRepository categoryRepository
+        )
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel piesViewModel = new PieListViewModel
+            {
+                Pies = _pieRepository.Pies,
+                CurrentCategory = "Cheese cakes"
+            };
+
+            return View(piesViewModel);
+        }
+    }
+}
