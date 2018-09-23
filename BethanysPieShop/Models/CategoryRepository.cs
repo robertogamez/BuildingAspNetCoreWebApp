cfbb39702646b6992b689a72ad59d32dabc08603
Roberto G <roberto.manuel.balderas.gamez@hotmail.com>
2018-09-23 18:03:28 -0500

    Building Your First ASP.NET Core Web Application - Primer commit del proyecto
    Se añade soporte para carrito de compras

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            appDbContext = context;
+        }
+
+        public IEnumerable<Category> Categories
+            => appDbContext.Categories;
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,19 +6,42 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BethanysPieShop
 {
     public class Startup
     {
+        private IConfigurationRoot configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(configurationRoot.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IPieRepository, PieRepository>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
             services.AddMvc();
+
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +50,10 @@ namespace BethanysPieShop
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvcWithDefaultRoute();
+
+            DbInitializer.Seed(app);
         }
     }
 }
